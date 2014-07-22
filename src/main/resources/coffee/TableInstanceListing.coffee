545661b6d39493c8ff1c7292a7be6d179b467ea0
rodrigovilar <rvilar@gmail.com>
2014-07-22 20:22:13 -0300

    Creating the default Class Widget: TableInstanceListing

---
+++ src/main/resources/coffee/TableInstanceListing.coffee
@@ -0,0 +1,49 @@
+class TableInstanceListing
+
+	init: (conf) ->
+		LOM.getJSON "api/data/class/#{conf.classFullName}/attributes", (attributes) =>
+			@drawTable(attributes, conf.classFullName)
+
+	drawTable: (attributesJson, classFullName) ->
+		@page = LOM.emptyPage()
+		table = $("<table>")
+		@page.append table
+		@buildTableHead(attributesJson, table);
+		LOM.getJSON "api/data/class/#{classFullName}/instances", (instances) =>
+			@buildTableBody(instances, attributesJson, table, classFullName)
+
+	buildTableHead: (attributesJson, table) ->
+		thead = $("<thead>");
+		table.append thead
+		trHead = $("<tr>");
+		trHead.attr "id", "attributes"
+		thead.append trHead
+		attributesJson.forEach (attribute) ->
+			thHead = $("<th>#{attribute.name}</th>")
+			thHead.attr "id", "attribute_" + attribute.id
+			trHead.append thHead
+
+	buildTableBody: (instancesJson, attributesJson, table, classFullName) ->
+		if(instancesJson.length > 0)
+			tbody = $("<tbody>");
+			tbody.attr "id", "instances"
+			table.append tbody
+			instancesJson.forEach (instance) =>
+				buildTableLine instance, attributesJson, tbody, classFullName
+		else
+			table.append "There are not instances"
+
+	buildTableLine: (instance, attributesJson, tbody, classFullName) ->
+		trbody = $("<tr>")
+		trbody.attr "id", "instance_" + instance.id
+		tbody.append trbody
+		attributesJson.forEach (attribute) =>
+			td  = $("<td>#{instance[attribute.name]}</td>" );
+			td.attr "id", "instance_" + instance.id + "_attribute_" + attribute.id
+			trbody.append td
+		trbody.click => 
+			LOM.loadScript 'api/widget/class/' + classFullName + '/edit',
+				classFullName: classFullName
+				id: instance.id
+
+return new TableInstanceListing
---
+++ src/main/webapp/js/TableInstanceListing.js
@@ -0,0 +1,83 @@
+(function() {
+  var TableInstanceListing;
+
+  TableInstanceListing = (function() {
+
+    function TableInstanceListing() {}
+
+    TableInstanceListing.prototype.init = function(conf) {
+      var _this = this;
+      return LOM.getJSON("api/data/class/" + conf.classFullName + "/attributes", function(attributes) {
+        return _this.drawTable(attributes, conf.classFullName);
+      });
+    };
+
+    TableInstanceListing.prototype.drawTable = function(attributesJson, classFullName) {
+      var table,
+        _this = this;
+      this.page = LOM.emptyPage();
+      table = $("<table>");
+      this.page.append(table);
+      this.buildTableHead(attributesJson, table);
+      return LOM.getJSON("api/data/class/" + classFullName + "/instances", function(instances) {
+        return _this.buildTableBody(instances, attributesJson, table, classFullName);
+      });
+    };
+
+    TableInstanceListing.prototype.buildTableHead = function(attributesJson, table) {
+      var thead, trHead;
+      thead = $("<thead>");
+      table.append(thead);
+      trHead = $("<tr>");
+      trHead.attr("id", "attributes");
+      thead.append(trHead);
+      return attributesJson.forEach(function(attribute) {
+        var thHead;
+        thHead = $("<th>" + attribute.name + "</th>");
+        thHead.attr("id", "attribute_" + attribute.id);
+        return trHead.append(thHead);
+      });
+    };
+
+    TableInstanceListing.prototype.buildTableBody = function(instancesJson, attributesJson, table, classFullName) {
+      var tbody,
+        _this = this;
+      if (instancesJson.length > 0) {
+        tbody = $("<tbody>");
+        tbody.attr("id", "instances");
+        table.append(tbody);
+        return instancesJson.forEach(function(instance) {
+          return buildTableLine(instance, attributesJson, tbody, classFullName);
+        });
+      } else {
+        return table.append("There are not instances");
+      }
+    };
+
+    TableInstanceListing.prototype.buildTableLine = function(instance, attributesJson, tbody, classFullName) {
+      var trbody,
+        _this = this;
+      trbody = $("<tr>");
+      trbody.attr("id", "instance_" + instance.id);
+      tbody.append(trbody);
+      attributesJson.forEach(function(attribute) {
+        var td;
+        td = $("<td>" + instance[attribute.name] + "</td>");
+        td.attr("id", "instance_" + instance.id + "_attribute_" + attribute.id);
+        return trbody.append(td);
+      });
+      return trbody.click(function() {
+        return LOM.loadScript('api/widget/class/' + classFullName + '/instance/' + instance.id, {
+          classFullName: classFullName,
+          id: instance.id
+        });
+      });
+    };
+
+    return TableInstanceListing;
+
+  })();
+
+  return new TableInstanceListing;
+
+}).call(this);
